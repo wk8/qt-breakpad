046de06ed78acbca09c87e8022827e89c836614a
Jean Rouge <jer329@cornell.edu>
2016-01-23 12:25:04 -0800

    Wrapper around `google_breakpad::ExceptionHandler::MinidumpCallback`

--- qt_breakpad.cpp
+++ qt_breakpad.cpp
@@ -7,17 +7,29 @@ void QtBreakpad::init(const QString& reportPath,
                       google_breakpad::ExceptionHandler::MinidumpCallback minidumpCallback,
                       void* callbackContext)
 {
-    // there can only be one handler
-    delete QtBreakpad::_instance;
+    QtBreakpad::replaceInstance(new QtBreakpad(reportPath, filterCallBack,
+                                               minidumpCallback, callbackContext));
+}
 
-    QtBreakpad::_instance = new QtBreakpad(reportPath, filterCallBack,
-                                               minidumpCallback, callbackContext);
+void QtBreakpad::init(const QString& reportPath,
+                      QMinidumpCallback qMinidumpCallback,
+                      void* callbackContext)
+{
+    QtBreakpad::replaceInstance(new QtBreakpad(reportPath, qMinidumpCallback, callbackContext));
 }
 
 QtBreakpad::QtBreakpad(const QString& reportPath,
                        google_breakpad::ExceptionHandler::FilterCallback filterCallBack,
                        google_breakpad::ExceptionHandler::MinidumpCallback minidumpCallback,
                        void* callbackContext)
+{
+    this->buildBreakpadHandler(reportPath, filterCallBack, minidumpCallback, callbackContext);
+}
+
+void QtBreakpad::buildBreakpadHandler(const QString& reportPath,
+                                      google_breakpad::ExceptionHandler::FilterCallback filterCallBack,
+                                      google_breakpad::ExceptionHandler::MinidumpCallback minidumpCallback,
+                                      void* callbackContext)
 {
 #if defined(Q_OS_WIN32)
     std::wstring pathAsStr = (const wchar_t*)reportPath.utf16();
@@ -42,7 +54,64 @@ QtBreakpad::QtBreakpad(const QString& reportPath,
 #endif
 }
 
+QtBreakpad::QtBreakpad(const QString& reportPath,
+           QMinidumpCallback qMinidumpCallback,
+           void* callbackContext)
+{
+    this->_qMinidumpContextWrapper = new QtBreakpad::QMinidumpContextWrapper(qMinidumpCallback, callbackContext);
+
+    this->buildBreakpadHandler(reportPath,
+                               NULL, // filter callback
+                               reinterpret_cast<google_breakpad::ExceptionHandler::MinidumpCallback>(&QtBreakpad::qMinidumpWrapper),
+                               this->_qMinidumpContextWrapper);
+}
+
+#if defined(Q_OS_WIN32)
+bool QtBreakpad::qMinidumpWrapper(const wchar_t* dump_path,
+                                  const wchar_t* minidump_id,
+                                  QMinidumpContextWrapper* contextWrapper,
+                                  EXCEPTION_POINTERS* exinfo,
+                                  MDRawAssertionInfo* assertion,
+                                  bool succeeded)
+{
+    Q_UNUSED(exinfo);
+    Q_UNUSED(assertion);
+#elif defined(Q_OS_LINUX)
+bool QtBreakpad::qMinidumpWrapper(const MinidumpDescriptor& descriptor,
+                                  QMinidumpContextWrapper* contextWrapper,
+                                  bool succeeded)
+{
+#endif
+    // if there's no dump file, not much to do
+    if (!succeeded) { return false; }
+
+    QMinidumpCallback qMinicudmpCallback = contextWrapper->qMinicudmpCallback;
+    void* context = contextWrapper->context;
+
+    QString minidumpFileName;
+
+#if defined(Q_OS_WIN32)
+    QDir minidumpDir = QDir(QString::fromWCharArray(dump_path));
+    minidumpFileName = minidumpDir.absoluteFilePath(QString::fromWCharArray(minidump_id) + ".dmp");
+#elif defined(Q_OS_LINUX)
+    minidumpFileName = descriptor.path();
+#endif
+
+    QFile minidumpFile(minidumpFileName);
+
+    return qMinicudmpCallback(minidumpFile, context);
+}
+
+void QtBreakpad::replaceInstance(QtBreakpad* newInstance)
+{
+    // there can only be one handler
+    delete QtBreakpad::_instance;
+
+    QtBreakpad::_instance = newInstance;
+}
+
 QtBreakpad::~QtBreakpad()
 {
     delete this->_breakpad_handler;
+    delete this->_qMinidumpContextWrapper;
 }
--- qt_breakpad.h
+++ qt_breakpad.h
@@ -3,6 +3,7 @@
 
 #include <QCoreApplication>
 #include <QDir>
+#include <QFile>
 #include <QProcess>
 #include <QString>
 
@@ -26,17 +27,60 @@ public:
                      google_breakpad::ExceptionHandler::MinidumpCallback minidumpCallback = NULL,
                      void* callbackContext = NULL);
 
+    typedef bool (*QMinidumpCallback)(QFile& minidumpFile, void* context);
+
+    static void init(const QString& reportPath,
+                     QMinidumpCallback qMinidumpCallback,
+                     void* callbackContext = NULL);
+
 private:
     Q_DISABLE_COPY(QtBreakpad)
     static QtBreakpad* _instance;
+    static void replaceInstance(QtBreakpad* newInstance);
+
+    class QMinidumpContextWrapper
+    {
+        public:
+            QMinidumpContextWrapper(QMinidumpCallback qMinicudmpCallback, void* context)
+            {
+                this->qMinicudmpCallback = qMinicudmpCallback;
+                this->context = context;
+            }
+
+            ~QMinidumpContextWrapper() { }
+
+            QMinidumpCallback qMinicudmpCallback;
+            void* context;
+    };
 
     QtBreakpad(const QString& reportPath,
                google_breakpad::ExceptionHandler::FilterCallback filterCallBack,
                google_breakpad::ExceptionHandler::MinidumpCallback minidumpCallback,
                void* callbackContext);
+    QtBreakpad(const QString& reportPath,
+               QMinidumpCallback qMinidumpCallback,
+               void* callbackContext);
     ~QtBreakpad();
 
     google_breakpad::ExceptionHandler* _breakpad_handler;
+    void buildBreakpadHandler(const QString& reportPath,
+                              google_breakpad::ExceptionHandler::FilterCallback filterCallBack,
+                              google_breakpad::ExceptionHandler::MinidumpCallback minidumpCallback,
+                              void* callbackContext);
+
+    QMinidumpContextWrapper* _qMinidumpContextWrapper;
+#if defined(Q_OS_WIN32)
+    static bool qMinidumpWrapper(const wchar_t* dump_path,
+                                 const wchar_t* minidump_id,
+                                 QMinidumpContextWrapper* context,
+                                 EXCEPTION_POINTERS* exinfo,
+                                 MDRawAssertionInfo* assertion,
+                                 bool succeeded);
+#elif defined(Q_OS_LINUX)
+    static bool qMinidumpWrapper(const MinidumpDescriptor& descriptor,
+                                 QMinidumpContextWrapper* context,
+                                 bool succeeded);
+#endif
 };
 
 #endif // QT_BREAKPAD_H
